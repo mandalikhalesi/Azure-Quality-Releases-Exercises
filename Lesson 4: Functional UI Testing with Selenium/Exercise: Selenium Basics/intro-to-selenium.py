097ace6c64a37b640b949a9c17f918c1bfb0c3a4
M Khalesi <mkhalesi@mandalikhalesi.com>
2024-05-30 21:41:19 +0900

    update code to support magento website as previous site unavailable

--- Lesson 4: Functional UI Testing with Selenium/Exercise: Selenium Basics/intro-to-selenium.py
+++ Lesson 4: Functional UI Testing with Selenium/Exercise: Selenium Basics/intro-to-selenium.py
@@ -1,7 +1,32 @@
-# #!/usr/bin/env python
-from selenium import webdriver
 
-# Start the browser and navigate to http://automationpractice.com/index.php.
+# Basic usage of Selenium from Selenium website: (https://github.com/SeleniumHQ/seleniumhq.github.io/blob/trunk/examples/python/tests/getting_started/first_script.py#L12-L13)
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 driver = webdriver.Chrome()
-driver.get('http://automationpractice.com/index.php')
-driver.find_element_by_css_selector("input[id='search_query_top']").send_keys("t shirt")
+
+URL = "https://magento.softwaretestingboard.com/"
+SEARCH_ITEM = "t-shirt"
+
+print("Navigating to: " + URL)
+driver.get(URL)
+
+title = driver.title
+
+driver.implicitly_wait(5)
+
+search_box = driver.find_element(by=By.ID, value="search")
+
+print("Search for: " + SEARCH_ITEM)
+search_box.send_keys(SEARCH_ITEM)
+submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button[class='action search']")
+submit_button.click()
+
+print("We are on the search page for: " + SEARCH_ITEM)
+
+results = driver.find_element(by=By.CSS_SELECTOR, value="p[class='toolbar-amount']")
+
+print("Results for the search: " + results.text + " were found.")
+
+driver.quit()
